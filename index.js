814afe6735fe89557dddd3cbb6daae8d0476cde4
Ashiqweb123 <rabiulazim430@gmail.com>
2023-05-10 00:09:41 -0700

    third-commit

--- index.js
+++ index.js
@@ -7,5 +7,29 @@ const loadData=()=>{
   const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
   fetch(url)
   .then(res=>res.json())
-  .then(data=>console.log(data))
+  .then(data=>displayData(data.data))
 }
+ const displayData= mobile=>
+ {
+    const cardContainer=document.getElementById('card-container')
+    for(const mobiles of mobile)
+    {
+        console.log(mobiles)
+        const div= document.createElement('div')
+        div.innerHTML=`
+        <div class="card mb-3" style="width: 15rem;">
+        <img src="${mobiles.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">"${mobiles.phone_name
+          }"</h5>
+          <p class="card-text">"${mobiles.slug
+          }"</p>
+          <a href="#" class="btn btn-primary">Details</a>
+        </div>
+      </div>
+        `
+        cardContainer.appendChild(div) 
+    }
+   
+ }
+
