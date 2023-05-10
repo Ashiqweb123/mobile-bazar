8f116d39866570b4fd49da6cc90208ba4d1a8f62
Ashiqweb123 <rabiulazim430@gmail.com>
2023-05-09 22:54:42 -0700

    api-fetch

---
+++ index.js
@@ -0,0 +1,11 @@
+const loadData=()=>{
+    const searchField=document.getElementById('search-field')
+   const searchText=searchField.value
+   
+  searchField.value =''
+
+  const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  fetch(url)
+  .then(res=>res.json())
+  .then(data=>console.log(data))
+}
