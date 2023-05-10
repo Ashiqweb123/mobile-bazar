6695739f0a9358230c63d9c9238e3e5af1a5e027
Ashiqweb123 <rabiulazim430@gmail.com>
2023-05-10 05:01:42 -0700

    details-commit

--- index.js
+++ index.js
@@ -38,8 +38,36 @@ const loadDataDetails=data=>
     const url=`https://openapi.programming-hero.com/api/phone/${data}`
 fetch(url)
 .then(res=>res.json())
-.then(data=>console.log(data))
+.then(data=>displayDataDetails(data.data))
     
 }
 
+
+ const displayDataDetails= detail =>
+ {
+  console.log(detail)
+
+  const cardDetails=document.getElementById('card-details')
+  const div= document.createElement('div')
+  div.innerHTML=`
+  <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${detail.image
+      }" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${detail.name}</h5>
+        <p class="card-text">Chipset: ${detail.mainFeatures.chipSet}</p>
+        <p class="card-text">Display-Size: ${detail.mainFeatures.displaySize}</p>
+        <p class="card-text">Memory: ${detail.mainFeatures.memory}</p>
+        <p class="card-text"><small class="text-body-secondary">Realise Date: ${detail.releaseDate}</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+  `
+  cardDetails.appendChild(div)
+ }
 
